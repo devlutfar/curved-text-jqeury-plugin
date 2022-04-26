e31662ee754313189d56233dbea9fe7fed1e85e0
md.lutfar rahman <96845210+devlutfar@users.noreply.github.com>
2022-04-26 16:38:23 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    //curved text jquery plugin start
+
+     var arc = function (t)
+     {
+         return {
+             x: 500.0 + 300.0 * Math.cos(2.0 * Math.PI * t - 0.5 * Math.PI),
+             y: 400.0 + 300.0 * Math.sin(2.0 * Math.PI * t - 0.5 * Math.PI)
+         };
+     };
+ 
+     $('#text1').curvedText({
+         curve:    arc,
+         domain:   [-0.2, 0.2],
+         viewport: { x: 0.0, y: 0.0, width: 1000.0, height: 400.0 }
+     });
+ 
+    //curved text jquery plugin end
+
+});
